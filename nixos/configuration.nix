8d01ca6a0fba2e10b7289300db107f2df382a45d
Daniel Stellmon <dstellm@gmail.com>
2023-07-31 01:02:39 -0500

    enabled gnupg and added font-awesome and google fonts

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -144,17 +144,20 @@
   programs.zsh.enable = true;
 
   #enable fonts
+  fonts.fondDir.enable = true;
   fonts.fonts = with pkgs; [
     nerdfonts
+    font-awesome
+    google-fonts
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
