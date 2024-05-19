b58f0f8f02ef32bc4fc656fe568e26efd7ddf339
Daniel Stellmon <Dstellm@gmail.com>
2024-05-18 20:28:22 -0500

    delete links for reset

--- delete.sh
+++ delete.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+rm -r ~/.config/kitty/ #archive
+rm -r ~/.config/nvim/ #archive
+rm -r ~/.config/nvim-writing/ #archive
+rm -r ~/.config/custScripts/ # remove folder
+rm ~/.config/starship.toml 
+rm -r ~/.config/terminator/
+rm -r~/.config/tmux/
+rm ~/.gitconig
