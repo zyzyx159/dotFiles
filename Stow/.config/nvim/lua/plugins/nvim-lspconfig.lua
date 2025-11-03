ab93513e72292284fe2361a71e199a0df1810be1
Daniel Stellmon <Dstellm@gmail.com>
2025-11-03 00:11:12 -0600

    unsaved changes

--- Stow/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ Stow/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -59,14 +59,14 @@ local config = function()
 	})
 
 	-- typescript
-	lspconfig.tsserver.setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-		filetypes = {
-			"typescript",
-		},
-		root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
-	})
+--	lspconfig.tsserver.setup({
+--		on_attach = on_attach,
+--		capabilities = capabilities,
+--		filetypes = {
+--			"typescript",
+--		},
+--		root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
+--	})
 
 	-- bash
 	lspconfig.bashls.setup({
