b8c56de1d949911d80f1a22d9fa1498d20d58eae
Daniel Stellmon <Dstellm@gmail.com>
2025-11-29 21:35:15 -0600

    added background color to supress error

--- Stow/.config/nvim/lua/plugins/noice.lua
+++ Stow/.config/nvim/lua/plugins/noice.lua
@@ -18,5 +18,11 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
+    opts = {
+      background_color = "#000000", -- Set background color to fix transparency issue
+      timeout = 2000,
+      top_down = false,
+      level =3,
+      },
     }
 }
